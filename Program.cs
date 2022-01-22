48f416bfdd3ad8f29598ea1eae568f61ae03c11c
smichelle-dev <dev.smichelle@gmail.com>
2022-01-22 20:26:05 -0300

    trabalhando com events em C#

--- Program.cs
+++ Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 
 
-namespace Circulo{
+namespace MeuApp
+{
     class Program
     {
-        static void Main(string[]args){
-            
-AreaCirculo.RecebeDados();        
+        
+        
+        static void Main(string[] args)
+        {
+       var sala =new Sala(3);
+       sala.SalaVendidaEvent += EmSalaCheia;
+       sala.ReservaDeAssentos();
+        sala.ReservaDeAssentos();
+         sala.ReservaDeAssentos();
+          sala.ReservaDeAssentos();
+          sala.ReservaDeAssentos();
         }
+
+        static void EmSalaCheia(object sender, EventArgs e){
+            Console.WriteLine("Sala Lotada");
+        }
+   
+   public class Sala
+   {
+       public Sala(int assentos){
+           Assentos = assentos;
+           SendoUsadas =0;
+       }
+
+       private int SendoUsadas =0;
+       public int Assentos{get;set;}
+
+       public void ReservaDeAssentos(){
+           SendoUsadas++;
+           if(SendoUsadas>=Assentos){
+               AoSalaVendida(EventArgs.Empty);
+           }else{
+               Console.WriteLine("Assento reservado");
+           }
+       }
+       public event EventHandler SalaVendidaEvent;
+       protected virtual void AoSalaVendida(EventArgs e){
+               EventHandler handler = SalaVendidaEvent;
+               handler?.Invoke(this,e );
+       }
+       
+   }
+
+
     }
+    
 }
+
+
+
